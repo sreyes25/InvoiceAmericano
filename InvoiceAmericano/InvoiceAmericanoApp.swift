9cc49c2e69021c4f6c47e77ca653b22642f4c49e
Sergio Reyes <82613689+sreyes25@users.noreply.github.com>
2025-09-17 15:44:29 -0400

    Initial Commit

---
+++ InvoiceAmericano/InvoiceAmericanoApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvoiceAmericanoApp.swift
+//  InvoiceAmericano
+//
+//  Created by Sergio Reyes on 9/17/25.
+//
+
+import SwiftUI
+
+@main
+struct InvoiceAmericanoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
